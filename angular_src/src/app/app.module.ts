40d33d3e8d65a7920825f38bbb19c0aee2c8dfa6
max <enrique.longton@gmail.com>
2018-10-13 13:47:32 -0400

    added form fields

--- angular_src/src/app/app.module.ts
+++ angular_src/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { AppRoutingModule } from './modules/app-routing.module';
+import { SignupComponent } from './users/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular_src/src/app/modules/app-routing.module.ts
+++ angular_src/src/app/modules/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { UsersComponent } from '../users/users.component';
+import { SignupComponent } from '../users/signup/signup.component';
 
 
 
 const appRoutes: Routes = [
   // {path: '', redirectTo: '/users',   pathMatch: 'full'},
-  {path: 'users', component: UsersComponent},
+  {path: 'users', component: UsersComponent, children: [
+    {path: 'signup', component: SignupComponent}
+  ]},
   // {path: 'organizations', component: OrganizationListComponent},
   // {path: 'programs', component: ProgramListComponent},
 ]
